e8a8d60c492be42a71cb84154039fdf90ef2bcf3
1547784788 <1547784788@qq.com>
2020-05-15 14:32:35 +0800

    2020.5.15.2

--- HistogramEqual/HistogramEqual.cpp
+++ HistogramEqual/HistogramEqual.cpp
@@ -2,10 +2,33 @@
 //
 
 #include <iostream>
-
+#include <opencv.hpp>
+using namespace cv;
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	Mat channels[3];
+	Mat dstchannels[3];
+
+
+	Mat dstMat;
+	Mat srcMat = imread("E:\\课程\\大二下\\数字图像处理\\etest.jpg");
+
+
+	split(srcMat, channels);
+	for (int i = 0; i < 3; i++)
+	{
+		equalizeHist(channels[i], dstchannels[i]);
+	}
+	merge(dstchannels,3 , dstMat);
+
+
+
+
+	imshow("dstMat",dstMat);
+	waitKey(0);
+	return 0;
+    //std::cout << "Hello World!\n";
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
